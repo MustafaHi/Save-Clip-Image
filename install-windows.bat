b17ac1fc3ea0e531153a232570ffe62ef281d850
Mustafa <matterik971@gmail.com>
2022-05-11 12:05:31 +0300

    Update install-windows.bat

--- install-windows.bat
+++ install-windows.bat
@@ -1,1 +1,1 @@
-curl -o SaveMemImage.exe -LO https://github.com/c-smile/sciter-sdk/raw/master/bin.win/x32/scapp.exe
+curl -o SaveImage.exe -LO https://github.com/c-smile/sciter-sdk/raw/master/bin.win/x32/scapp.exe
